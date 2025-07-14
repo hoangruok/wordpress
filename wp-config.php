64dbed998b496839466d16d1baddc3bdaa001dfb
hoangruok <vhhsky2k6@gmail.com>
2025-07-14 20:10:24 +0700

    course-key

--- wp-config.php
+++ wp-config.php
@@ -21,21 +21,15 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'wordpress' );
-
-/** Database username */
 define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'localhost:3307' );
 define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+define( 'WP_HOME', 'http://localhost/wordpress' );
+define( 'WP_SITEURL', 'http://localhost/wordpress' );
+
+
 
 /**#@+
  * Authentication unique keys and salts.
